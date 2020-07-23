369f6ec4f29039be288e0e5bca5ec72cb4f48a35
LuisRivero021298 <luisda021298@gmail.com>
2020-07-23 16:52:49 -0500

    Show labels filter and clear whit btn clear

--- src/App.js
+++ src/App.js
@@ -4,12 +4,38 @@ import CardGrid from "./components/CardGrid";
 
 const App = () => {
   const [categories, setCategories] = useState([""]);
+
+  console.log(categories);
+  const LabelsSelected = () => {
+    const handleClearFilter = () => {
+      setCategories([""]);
+    };
+
+    return (
+      <>
+        <article className="show-labels-selected card-job">
+          <div className="card-labels">
+            {categories[0].map((category) => (
+              <div key={category + 2} className="card-label">
+                <div>{category}</div>
+              </div>
+            ))}
+          </div>
+          <div className="clear-labels">
+            <button onClick={handleClearFilter}>Clear</button>
+          </div>
+        </article>
+      </>
+    );
+  };
+
   return (
     <>
       <header className="App-header">
         <img src="images/bg-header-mobile.svg" alt="bg-header" />
       </header>
       <main className="App-main">
+        {categories[0] === "" ? null : <LabelsSelected />}
         {categories.map((category) => (
           <CardGrid
             key={category}
